a43e7b25caa973c32d3d857e557dae104e0bdfa0
siddhi1305-bit <siddhisapkal2711@gmail.com>
2026-05-17 10:53:02 +0530

    Create 1_ database_exploration.sql

---
+++ scripts/1_ database_exploration.sql
@@ -0,0 +1,17 @@
+
+-- Retrieve a list of all tables in the database
+SELECT 
+TABLE_CATALOG,
+TABLE_SCHEMA,
+TABLE_NAME,
+TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- Retrieve all columns for a specific table (dim_customers)
+SELECT
+COLUMN_NAME,
+IS_NULLABLE,
+CHARACTER_MAXIMUM_LENGTH,
+DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
